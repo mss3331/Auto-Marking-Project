cf3c4e8e6b4b378b9d5a6f7b3211ff76ec4cae1f
Mahmood_Haithami <mss3331@hotmail.com>
2021-11-18 20:04:08 +0800

    At this moment the project is ready to be used for other exercises.

--- AutoMark.py
+++ AutoMark.py
@@ -2,6 +2,8 @@ import subprocess
 import yaml
 import os
 import pandas as pd
+import shutil
+from datetime import datetime
 from difflib import SequenceMatcher
 #Check submission CSF-Ex1-20213725 and CSF-Ex1-20307078 there is a mismatch ()
 
@@ -17,12 +19,16 @@ def getDirOrFile(dir_name, DirEntry_iterator, threshold=0.8):
     target_dir=DirEntry object: if the dir_name match one of the directory's name in the list. And None otherwise.
     """
     DirEntry_list = list(DirEntry_iterator)
-    similarity_list = [(SequenceMatcher(None, dir_name.lower(), dir_entry.name.lower()).ratio()) for dir_entry in DirEntry_list]
+    similarity_list = [(SequenceMatcher(None, dir_name, dir_entry.name).ratio()) for dir_entry in DirEntry_list]
     if len(similarity_list)==0:
         return None
 
     max_index = similarity_list.index(max(similarity_list))
 
+    # print("threshold is ", threshold)
+    # print(similarity_list)
+    # print(DirEntry_list)
+    # exit(0)
     if (similarity_list[max_index]>=threshold):
         return DirEntry_list[max_index]
     else:# if nothing is found
@@ -45,6 +51,7 @@ def markFile(file_path, emulator_name):
     if(cmd_output.find("Comparison ended success")>=0):
         is_successful = True
         feedback = SUCCESS
+
 
     return is_successful, feedback.rstrip('\n') # "End of ... succussfuly\n" --> "End of ... succussfuly"
 
@@ -72,10 +79,11 @@ def checkStructure():
     # get list of all directories
     dir_list = os.scandir()
 
-    submissions_dir = getDirOrFile("submissions",dir_list)
+    submissions_dir = getDirOrFile("Excer1 - those who achieved zero",dir_list, threshold=0.5)
 
     if not submissions_dir:# if submissions_dir is None
         printError("submissions folder is not found in the current directory!!\n %s "%root)
+
 
     return root,submissions_dir
 
@@ -84,7 +92,7 @@ def printError(message):
     input("Press Enter to Exit...")
     exit(-1)
 
-def _searchForDirOrFile(name,current_dir):
+def _searchForDirOrFile(name,current_dir, threshold=0.8):
     """This is a recursive function that search for a specific directory in a breadth first fashion
     (i.e. search first for the name in the current_dir then go deeper)
     :parameter
@@ -95,7 +103,7 @@ def _searchForDirOrFile(name,current_dir):
     """
     target_dir=None
     dir_iterator= os.scandir(current_dir)
-    found = getDirOrFile(name,dir_iterator)
+    found = getDirOrFile(name,dir_iterator, threshold= threshold)
 
     if found:#if we found the name in the current_dir return
         target_dir = found
@@ -105,7 +113,7 @@ def _searchForDirOrFile(name,current_dir):
     dir_iterator= os.scandir(current_dir)
     for dir_temp in dir_iterator:
         if dir_temp.is_dir():
-            found=_searchForDirOrFile(name, dir_temp)
+            found=_searchForDirOrFile(name, dir_temp, threshold= threshold)
             if found:  # if we found the name in the current_dir return
                 target_dir = found
                 return target_dir
@@ -115,8 +123,8 @@ def _searchForDirOrFile(name,current_dir):
 def _checkRequiredFiles(required_files_list, target_folder_name,url_of_our_tst,student_dir):
     """
     This function do two things:
-     1- check the existence of required_files_list and add a penalty if a file is missing
-     2- copy our .tst files to target_folder_name
+     1- check the existence of required_files_list and add a penalty if a file is missing.
+
     :param required_files_list: list of all required files
     :param target_folder_name: which folder to 1- search for the required files, 2- to copy our .tst files
     :param url_of_our_tst: where to fetch our .tst files into the student's submission folder
@@ -145,13 +153,15 @@ def _checkRequiredFiles(required_files_list, target_folder_name,url_of_our_tst,s
                         pass
 
     else:# if we didn't find the specified folder then search for the required file in sub-directories
+        # print("we passed because we didn't find target_dir")
+        # exit(0) #I think I should copy our .tst files here. check if there is a folder name for the task. If not become strict and give zero or I can do that by file bases
         pass
     # case two, we have only one section for the excersise, hence, we may not have a target_folder_name (i.e. target_folder_name='')
     if total_penalties == 0:
         all_feedbacks=""
     return total_penalties, all_feedbacks
 
-def mark_submission(student_dir):
+def mark_submission(student_dir, use_our_tst=False):
     """This method is responsible for doing the marking for each section in the exercise for one student.
     It uses the global variable "configurations" defined in the __main__ to get essential information
     :parameter
@@ -174,7 +184,7 @@ def mark_submission(student_dir):
          1- Check that all required files exist, if not, add a penalty and insert an empty file to prevent
             invoking build-in files
          2- Load our .tst files to the specified folder
-            step 1 and 2 are done in method "checkRequiredFiles"
+        Step 1 and 2 are done in method "checkRequiredFiles"
         """
         required_files_list = configurations[section]["Check_Files"]
         target_folder_name = configurations[section]["Folder_Name"]
@@ -194,13 +204,25 @@ def mark_submission(student_dir):
             file_name, mark = tst_mark_yml.split(" ") # Mult.tst 20 --> tst="Mult" and mark="20"
             mark = int(mark) # mark = "20" --> 20
 
-            target_folder_path = _searchForDirOrFile(target_folder_name,student_dir) # search for specific path
-            if not target_folder_path: #in case we didn't the specified folder within the student submission, use the student directory instead
-                target_folder_path = student_dir
-            tst_file = _searchForDirOrFile(file_name,target_folder_path)
+            if target_folder_name: #if there is exist folder name, search for it
+                target_folder_path = _searchForDirOrFile(target_folder_name,student_dir) # search for specific path
+            else: target_folder_path = student_dir # otherwise use the student submission folder
+
+
+            tst_file = _searchForDirOrFile(file_name,target_folder_path,threshold=1)
 
             if tst_file:
+                #here we either copy our .tst or use the student's .tst
+                if use_our_tst:
+                    #students MyNot.tst path. #tst_file.path is actually path+name
+                    tst_path = tst_file.path.split(tst_file.name)[0]
+                    #copy .tst and .cmp from source to students folder
+                    shutil.copy2(os.path.join(url_of_our_tst, file_name), tst_path)
+                    compare_name = file_name.split('.')[0] + ".cmp" # Mult.tst -> Mult -> Mult.cmp
+                    shutil.copy2(os.path.join(url_of_our_tst, compare_name), tst_path)
+
                 is_success, feedback = markFile(tst_file.path,emulator_name)
+
                 marks_dic[file_name] = 0
                 feedbacks_dic[file_name]= feedback
                 if is_success:
@@ -227,31 +249,102 @@ def mark_submission(student_dir):
 
     return student_name, one_row_result
 
-#global variables
-# Constant variable
-SUCCESS = "success"
 
-root, submissions_dir= checkStructure()
-configurations=getYAMLConfig()
-all_submissions = os.scandir(submissions_dir)
+def save_to_excel(all_results, extra='' ):
+    df = pd.DataFrame(data=all_results).T
+    now = datetime.now()
+    dt_string = now.strftime("%d-%m-%Y %H-%M-%S")
 
-def run():
+
+
+    df.to_excel(root_folder_name + '_Marks_' + extra + dt_string + '.xlsx')
+
+
+def run(use_our_tst):
+    ''' The mode variable should have the value "" or "strict"
+    if "strict" ->
+        1- The folder and file names should match 100%
+        2- Copy our .tst files to the students folder first
+    '''
     all_results = {}
 
     # For each student aka submission, do
     for student_dir in all_submissions:
         # submissions folder may contain non folders (e.g. submissions.rar that contain all submissions will be skipped)
         if student_dir.is_dir():
-            student_name, one_row_result = mark_submission(student_dir)
+            student_name, one_row_result = mark_submission(student_dir, use_our_tst)
             all_results[student_name] = one_row_result
         
     
-    root_folder_name = root.split("\\")[-1]
-    df = pd.DataFrame(data=all_results).T
-    df.to_excel(root_folder_name + '_Marks.xlsx')
 
-    input(("="*70)+"\nFinished Marking All Students, Press Enter to exit")
+
+    #save results in excel file
+    if use_our_tst:
+        ourTst = "ourTst_"
+    else:
+        ourTst = "studentTst_"
+    save_to_excel(all_results, extra=ourTst)
+
+    return all_results
+
+
+def unzipSubmissions(all_submissions):
+    for student_dir in all_submissions:
+        # submissions folder may contain non folders (e.g. submissions.rar that contain all submissions will be skipped)
+        if student_dir.is_dir():
+            zip_file = getDirOrFile('.zip',os.scandir(student_dir),threshold=0)
+            if zip_file.name.find('.zip')>=0: #if file name is .zip that mean it is a zip file
+                # print(zip_file)
+                shutil.unpack_archive(zip_file,zip_file.path.split(zip_file.name)[0])
+                os.remove(zip_file)
+
+
+
+
+
 if __name__ == '__main__':
-    run()
+    # global variables & Constant variable
+    SUCCESS = "success"
+    root, submissions_dir = checkStructure()
+    root_folder_name = root.split("\\")[-1]
+
+    configurations = getYAMLConfig()
+
+
+    all_submissions = list(os.scandir(submissions_dir))
+    #unzip submissions
+    unzipSubmissions(all_submissions)
+
+    results_student_tst = run(use_our_tst= False)
+
+    print("\n------------- Now we will marking using our .tst files -------------")
+    try:
+        shutil.copytree(submissions_dir.path,submissions_dir.path+"_withOurTstScript")
+    except:
+        print("Notification:\n",submissions_dir.path+"_withOurTstScript"+" folder was created from a previous run."
+                                                                         "Hence, we will delete it and copy again\n")
+        shutil.rmtree(submissions_dir.path+"_withOurTstScript")
+        try:
+            shutil.copytree(submissions_dir.path, submissions_dir.path + "_withOurTstScript")
+        except: printError("Cannot copy the content of "+submissions_dir.path+" to run our script using our .tst files")
+
+    all_submissions = list(os.scandir(submissions_dir.path+"_withOurTstScript"))
+    results_our_tst = run(use_our_tst=True)
+
+    #create average marking
+    average_mark = {}
+    for student_name in results_student_tst.keys():
+        student_tst_mark = results_student_tst[student_name]['Total']
+        our_tst_mark = results_our_tst[student_name]['Total']
+        avg = (student_tst_mark + our_tst_mark)/2
+        diff = student_tst_mark - our_tst_mark
+        average_mark[student_name] = {"Student .tst":student_tst_mark,
+                                      "Our .tst":our_tst_mark,
+                                      "difference":diff,
+                                      "Average":avg}
+
+    save_to_excel(average_mark, extra="average_")
+    input(("=" * 70) + "\nFinished Marking All Students, Press Enter to exit")
+    # run("strict")
 
 
